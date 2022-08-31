0f435bdedb61b4d8b54a31789f48adc8d2402f11
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2022-08-31 20:56:48 -0300

    Add support to NonEmptyList

---
+++ arrow/src/main/kotlin/com/github/ljtfreitas/julian/k/arrow/NonEmptyListResponseT.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2021 Tiago de Freitas Lima
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.ljtfreitas.julian.k.arrow
+
+import arrow.core.NonEmptyList
+import arrow.core.Option
+import arrow.core.nel
+import arrow.core.toOption
+import com.github.ljtfreitas.julian.Arguments
+import com.github.ljtfreitas.julian.Endpoint
+import com.github.ljtfreitas.julian.JavaType
+import com.github.ljtfreitas.julian.Promise
+import com.github.ljtfreitas.julian.Response
+import com.github.ljtfreitas.julian.ResponseFn
+import com.github.ljtfreitas.julian.ResponseT
+import java.lang.reflect.Type
+import java.lang.reflect.WildcardType
+import java.util.stream.Stream
+
+class NonEmptyListResponseT : ResponseT<Collection<Any>, NonEmptyList<Any>> {
+
+    override fun test(endpoint: Endpoint) = endpoint.returnType().`is`(NonEmptyList::class.java)
+
+    override fun adapted(endpoint: Endpoint): JavaType = endpoint.returnType().parameterized()
+        .map(JavaType.Parameterized::firstArg)
+        .map(::argument)
+        .orElseGet { Any::class.java }
+        .let { JavaType.parameterized(Collection::class.java, it) }
+
+    private fun argument(type: Type) : Type =
+        if (type is WildcardType && type.lowerBounds.isNotEmpty())
+            argument(type.lowerBounds.first())
+        else type
+
+    override fun <A> bind(endpoint: Endpoint, next: ResponseFn<A, Collection<Any>>) = object : ResponseFn<A, NonEmptyList<Any>> {
+
+        override fun run(response: Promise<out Response<A?>>, arguments: Arguments): Promise<NonEmptyList<Any>> =
+            next.run(response, arguments).then { NonEmptyList.fromListUnsafe(it.toList()) }
+
+        override fun returnType(): JavaType = next.returnType()
+    }
+}
